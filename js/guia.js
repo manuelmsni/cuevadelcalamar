24253ebc277a22241d0dd9f68aa47fa0b1da3020
manuelmsni <manuelmsni@hotmail.com>
2021-02-28 00:53:17 +0100

    Update guia.js

--- js/guia.js
+++ js/guia.js
@@ -1,1 +1,16 @@
-hola
+        
+        $('g-s-1').click(showGh1); 
+
+        function showGh1()
+          {
+          $(".g-s-1, .g-hidder-1").fadeIn('slow');
+          $(".g-shower-1").css("display", "none");
+          }
+
+        $('g-d-1').click(hideGh1); 
+
+        function hideGh1()
+          {
+          $(".g-shower-1").fadeIn('slow');
+          $(".g-s-1, .g-hidder-1").css("display", "none");
+          }
