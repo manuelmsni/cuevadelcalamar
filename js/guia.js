5d343ebf1e6ee00c687ca8efb017f72611fe83e9
manuelmsni <manuelmsni@hotmail.com>
2021-02-28 01:08:24 +0100

    Update guia.js

--- js/guia.js
+++ js/guia.js
@@ -27,14 +27,14 @@ function showOscurecerMenu() {
 
 //mostrar y ocultar elementos
 
-$('g-s-1').click(showGh1); 
+$('.g-s-1').click(showGh1); 
 
 function showGh1() {
  $(".g-s-1, .g-hidder-1").fadeIn('slow');
  $(".g-shower-1").css("display", "none");
  }
 
-$('g-d-1').click(hideGh1); 
+$('.g-d-1').click(hideGh1); 
 
 function hideGh1() {
   $(".g-shower-1").fadeIn('slow');
