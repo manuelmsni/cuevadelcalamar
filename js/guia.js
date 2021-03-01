93003ed4f2af7b0ec19911168c0e1b688ee8894b
manuelmsni <manuelmsni@hotmail.com>
2021-03-01 18:00:36 +0100

    Update guia.js

--- js/guia.js
+++ js/guia.js
@@ -64,3 +64,24 @@ function hideGh1() {
   $("#g-shower-1").fadeIn('slow');
   $("#g-h-1, #g-hidder-1").css("display", "none");
   }
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
