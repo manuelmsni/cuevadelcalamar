2143071f0e868821fdd2db0e66aba30c6e541385
manuelmsni <manuelmsni@hotmail.com>
2021-03-02 17:33:05 +0100

    Update guia.js

--- js/guia.js
+++ js/guia.js
@@ -65,23 +65,28 @@ function hideGh1() {
   $("#g-h-1, #g-hidder-1").css("display", "none");
   }
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          600:{
+              items:3,
+              nav:false
+          },
+          1000:{
+              items:5,
+              nav:true,
+              loop:false
+          }
+      }
 })
+});
+
+
