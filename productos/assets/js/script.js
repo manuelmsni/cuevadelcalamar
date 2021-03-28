77a3ff3e5fa366810446bd9ec3b0dff212a1c784
manuelmsni <manuelmsni@hotmail.com>
2021-03-28 13:02:56 +0200

    Update script.js

--- productos/assets/js/script.js
+++ productos/assets/js/script.js
@@ -209,8 +209,10 @@ function code() {
   else {
   vientre = XX;
   }
+  /*
   codigo = $( $("#tituloproducto").attr('name') + $(".colors.active .colorSRC1").find('option:selected').attr('name') + $(".colors.active .colorSRC2").find('option:selected').attr('name') + $(".colors.active .colorSRC3").find('option:selected').attr('name') + $("#vientreSRC").find('option:selected').attr('name'));
-  codigo = $( $("#tituloproducto").attr('name') + $(".colors.active .colorSRC1 option:selected").attr('name') + $(".colors.active .colorSRC2 option:selected").attr('name') + $(".colors.active .colorSRC3 option:selected").attr('name') + $("#vientreSRC option:selected").attr('name'));
+  */
+  codigo = $( $("#tituloproducto").attr('name') + $(".colors.active .colorSRC1 option:selected").attr('name') + $(".colors.active .colorSRC2 option:selected").attr('name') + $(".colors.active .colorSRC3 option:selected").attr('name') + vientre );
   $("#productID").text(codigo);
 }
 
