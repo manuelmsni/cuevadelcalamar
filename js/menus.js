91707cbc8ac0a181a09a4e05f027aea400e92fed
manuelmsni <manuelmsni@hotmail.com>
2021-04-17 17:15:36 +0200

    Update menus.js

--- js/menus.js
+++ js/menus.js
@@ -1,1 +1,26 @@
+//abrir y cerrar menu de la izquierda
 
+function showMenu() {
+  var x = document.getElementById("menu-izquierda");
+  if (x.style.left != "0px") {
+    x.style.left = "0px";
+  } else {
+    x.style.left = "-100%";
+  }
+}
+function showCloseMenu() {
+  var x = document.getElementById("cerrar-menu-izquierda");
+  if (x.style.left != "366px") {
+    x.style.left = "366px";
+  } else {
+    x.style.left = "-1000px";
+  }
+}
+function showOscurecerMenu() {
+  var x = document.getElementById("menu-izquierda-oscurecer");
+  if (x.style.left != "0px") {
+    x.style.left = "0px";
+  } else {
+    x.style.left = "-100%";
+  }
+}
