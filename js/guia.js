ef580227359d6947d5ab87725e8cfff879e81135
manuelmsni <manuelmsni@hotmail.com>
2021-04-17 18:25:12 +0200

    Update guia.js

--- js/guia.js
+++ js/guia.js
@@ -10,16 +10,3 @@ function hideGh1() {
   $("#g-shower-1").fadeIn('slow');
   $("#g-h-1, #g-hidder-1").css("display", "none");
 }
-
-  //Carrousel 1
-
-  let slider = tns({
-    container: '#slider-egis-1',
-    items: 5,
-    slideBy: 'page',
-    autoplay: false,
-    mouseDrag: true,
-    swipeAngle: false,
-    nav: false,
-    "loop": false,
-  });
