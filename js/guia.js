a9e9bdd4969b1111d90dd187611b5c5298134096
manuelmsni <manuelmsni@hotmail.com>
2021-04-21 18:04:33 +0200

    Update guia.js

--- js/guia.js
+++ js/guia.js
@@ -10,3 +10,10 @@ function hideGh1() {
   $("#g-shower-1").fadeIn('slow');
   $("#g-h-1, #g-hidder-1").css("display", "none");
 }
+
+function ShowHide(MostrarClase, OcultarClase) {
+  var Mostrar = document.getElementByClass(MostrarClase);
+  var Ocultar = document.getElementByClass(OcultarClase);
+  Mostrar.fadeIn('slow');
+  Ocultar.css("display", "none");
+}
