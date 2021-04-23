f1160a0b20daab0bed2f6882b0644040bfa25662
manuelmsni <manuelmsni@hotmail.com>
2021-04-23 19:22:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -129,33 +129,6 @@ function scrollRotate2() {
   image.style.transform = "rotate(" + window.pageYOffset / speed + "deg)";
 }
 
-//abrir y cerrar menu de la izquierda
-
-function showMenu() {
-  var x = document.getElementById("menu-izquierda");
-  if (x.style.left != "0px") {
-    x.style.left = "0px";
-  } else {
-    x.style.left = "-100%";
-  }
-}
-function showCloseMenu() {
-  var x = document.getElementById("cerrar-menu-izquierda");
-  if (x.style.left != "366px") {
-    x.style.left = "366px";
-  } else {
-    x.style.left = "-1000px";
-  }
-}
-function showOscurecerMenu() {
-  var x = document.getElementById("menu-izquierda-oscurecer");
-  if (x.style.left != "0px") {
-    x.style.left = "0px";
-  } else {
-    x.style.left = "-100%";
-  }
-}
-
 function SrcSet(ImageId, ThisElementSRC) {
   var element = document.getElementById(ImageId);
   element.src = ThisElementSRC;
