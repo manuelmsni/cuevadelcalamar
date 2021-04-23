330a2415c2ce1ea4f8204f55f6c6633421a2c261
manuelmsni <manuelmsni@hotmail.com>
2021-04-23 19:14:35 +0200

    Update menus.js

--- js/menus.js
+++ js/menus.js
@@ -2,13 +2,12 @@
 
 function showMenu() {
   var x = document.getElementById("menu-izquierda");
-  var hamburger = url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 0.5%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
   if (x.style.left != "0px") {
     x.style.left = "0px";
      $(".navbar-toggler-icon").css("background-image", "url('https://cuevadelcalamar.com/img/cerrar.png')");
   } else {
     x.style.left = "-100%";
-    $(".navbar-toggler-icon").css("background-image", hamburger);
+    $(".navbar-toggler-icon").css("background-image", "url('https://cuevadelcalamar.com/img/hamburguesa.png')");
   }
 }
 function showCloseMenu() {
